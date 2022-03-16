a2d2c124b6c96fac65c91ca569c3aecbb6050a1d
Cheng Yu <yucheng618@gmail.com>
2022-03-15 22:14:29 -0400

    axios

--- assignment/pokeman/src/component.js/Pokeman.js
+++ assignment/pokeman/src/component.js/Pokeman.js
@@ -1,28 +1,31 @@
 import React,{useState, useEffect} from "react";
+import axios from 'axios';
 
 
 
 const Pokeman = (props)=> {
     const [pokemons, setPokemons] = useState([]);
+    const [display, setDisplay] = useState(false);
 
-
+    useEffect(()=>{
+        axios.get('https://pokeapi.co/api/v2/pokemon?limit=1500')
+            .then(res=>{setPokemons(res.data.results)})
+            .catch(err=>console.log(err));
+    },[]);
 
     const handleSubmit = ()=> {
-        fetch("https://pokeapi.co/api/v2/pokemon?limit=1500")
-        .then(res=>res.json())
-        .then(res=>{
-            setPokemons(res.results);
-        })
-        .catch(err=>console.log(err));
-    }
+        setDisplay(true);
+}
+
+    
 
     return(
         <div>
             <button type="submit" onClick={handleSubmit}>Fetch Pokemon</button>
             <ul>
-                {pokemons.map((item,i)=>{
+                {display ? pokemons.map((item,i)=>{
                     return <li key={i}>{item.name}</li>
-                })}
+                }): ""}
             </ul>
         </div>
     )
