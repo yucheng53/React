98cd83c53a5b4465b405504b13a6bbe8d93a8ed8
Cheng Yu <yucheng618@gmail.com>
2022-03-22 12:30:41 -0400

    update validations

--- assignment/Jokes/server/models/jokes.model.js
+++ assignment/Jokes/server/models/jokes.model.js
@@ -4,14 +4,14 @@ const JokesSchema = new mongoose.Schema({
     setup:{
         type: String,
         require: [true, "Setup is required"],
-        minlength: [3, "Setup must be at least 5 characters long"]
+        minlength: [3, "Setup must be at least 3 characters long"]
     },
     punchline:{
         type: String,
         require: [true, "Setup is required"],
-        minlength: [3, "Setup must be at least 5 characters long"]
+        minlength: [3, "Setup must be at least 3 characters long"]
     }
-})
+},{timestamps: true})
 
 const Joke = mongoose.model("Joke", JokesSchema);
 
